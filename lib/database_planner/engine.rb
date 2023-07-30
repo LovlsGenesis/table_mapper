8047ad2a2519786fb244cbd0607e09c62dfad0e6
Guillaume Zeldine <g.zeldine@hotmail.fr>
2023-07-29 22:04:39 -0300

    fix layout stylesheet css

--- lib/database_planner/engine.rb
+++ lib/database_planner/engine.rb
@@ -1,5 +1,9 @@
 module DatabasePlanner
   class Engine < ::Rails::Engine
     isolate_namespace DatabasePlanner
+
+    initializer 'assets' do |app|
+      app.config.assets.precompile += %w( database_planner_manifest database_planner/application.css database_planner/application.js)
+    end
   end
 end
